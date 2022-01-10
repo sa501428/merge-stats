3da042cb648840b55f04a318f0e4f39a20e55a09
Muhammad S Shamim <sa501428@gmail.com>
2022-01-09 18:46:41 -0600

    update usage

--- src/sfh/DumpStats.java
+++ src/sfh/DumpStats.java
@@ -3,9 +3,18 @@ package sfh;
 import javastraw.reader.Dataset;
 import javastraw.tools.HiCFileTools;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
 public class DumpStats {
     public static void main(String[] args) {
         Dataset ds = HiCFileTools.extractDatasetForCLT(args[0], false, false);
-        System.out.println(ds.getStatistics());
+        try {
+            BufferedWriter out = new BufferedWriter(new FileWriter(args[1]));
+            out.write(ds.getStatistics());
+            out.close();
+        } catch (Exception e) {
+            System.err.println("Unable to print stats: " + e.getLocalizedMessage());
+        }
     }
 }
