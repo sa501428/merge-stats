7bcddf34c1684cc538f8a42b1ec3293524abac1e
Muhammad S Shamim <sa501428@gmail.com>
2022-01-09 21:58:55 -0600

    update usage

--- src/sfh/DumpStats.java
+++ src/sfh/DumpStats.java
@@ -8,15 +8,15 @@ import java.io.FileWriter;
 
 public class DumpStats {
     public static void main(String[] args) {
-        Dataset ds = HiCFileTools.extractDatasetForCLT(args[0], false, false);
+        Dataset ds = HiCFileTools.extractDatasetForCLT(args[1], false, false);
         try {
-            BufferedWriter out = new BufferedWriter(new FileWriter(args[1]));
+            BufferedWriter out = new BufferedWriter(new FileWriter(args[0]));
             out.write(ds.getStatistics());
             //System.out.println(ds.getGraphs());
             out.close();
         } catch (Exception e) {
             System.err.println("Unable to print stats: " + e.getLocalizedMessage());
-            System.err.println("Usage: java -jar stats-from-hic.jar <in.hic> <inter_stem>");
+            System.err.println("Usage: java -jar stats-from-hic.jar <inter_stem> <in.hic>");
         }
     }
 }
